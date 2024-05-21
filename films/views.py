6224e3caed90aa7e8eeb59d1c74cf0e99d10ec04
phi.nguyen <phi.nguyen@niteco.se>
2024-05-21 14:12:27 +0700

    Add film list

--- films/views.py
+++ films/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.db.models.query import QuerySet
+from django.shortcuts import render, redirect
 from django.http.response import HttpResponse, HttpResponsePermanentRedirect
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
 from django.contrib.auth import get_user_model
+from django.views.generic.list import ListView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from films.forms import RegisterForm
+from films.models import Film
 
 
 # Create your views here.
@@ -27,6 +32,20 @@ class RegisterView(FormView):
         return super().form_valid(form)
 
 
+class FilmList(ListView):
+    template_name = "films.html"
+    model = Film
+    context_object_name = "films"
+
+    def get_queryset(self) -> QuerySet[Film]:
+        user = self.request.user
+        if user.is_authenticated:
+            return user.films.all()
+        else:
+            return HttpResponseRedirect(reverse("login"))
+
+
+# HTMX handles
 def check_username(request):
     username = request.POST.get("username")
     if username == "":
@@ -39,5 +58,16 @@ def check_username(request):
         return HttpResponse(
             "<div id='username-error' class='primary' >This username is available</div>"
         )
-class Myfilms(TemplateView):
-    template_name ="films.html"
+
+
+def add_film(request):
+    name = request.POST.get("filmname")
+
+    film = Film.objects.create(name=name)
+
+    # add the film to the user's list
+    request.user.films.add(film)
+
+    # return template with all of the user's films
+    films = request.user.films.all()
+    return render(request, "partials/film-list.html", {"films": films})
--- htmx/settings.py
+++ htmx/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     "widget_tweaks",
     "django_extensions",
     "films",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -129,3 +131,7 @@ AUTH_USER_MODEL = "films.User"
 
 LOGIN_REDIRECT_URL = "index"
 LOGOUT_REDIRECT_URL = "index"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
